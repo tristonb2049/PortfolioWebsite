0d51a27076e343dfb8cc9070b04b1fb7cead64a9
Triston Blaney <tblaney97@comcast.net>
2019-12-06 00:57:18 -0600

    First commit

---
+++ public/js/firebase.js
@@ -0,0 +1,13 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCblyNHq9NZCFp3ogq3Na692_y52n3_EqA",
+    authDomain: "tristonblaney-434bf.firebaseapp.com",
+    databaseURL: "https://tristonblaney-434bf.firebaseio.com",
+    projectId: "tristonblaney-434bf",
+    storageBucket: "tristonblaney-434bf.appspot.com",
+    messagingSenderId: "365535857702",
+    appId: "1:365535857702:web:6ea4000d06fcb6235ceae7"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
